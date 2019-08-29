e4632f92271b71d5e6e3c5170a71bf51f659441b
Vanessa_Arce <vanessa.arce12@yahoo.com>
2019-08-29 10:31:38 -0400

    implemented lagagues details scripts

--- public/scripts/details.js
+++ public/scripts/details.js
@@ -9,7 +9,7 @@ $(function () {
         let obj;
         obj = data;
         teamDetails(obj)
-        playersDetails(obj,TeamId)
+        playersDetails(obj, TeamId)
     });
 
     // Necesito ver si esto esta bien
@@ -21,25 +21,18 @@ $(function () {
 
 
 }); // end of ready fuction
+
 //Table Body
 function teamDetails(obj) {
     let details =
-        "<tr><td class='font-weight-bold'> Team Id:</td><td>" +
-        obj.TeamId + "</td></tr>" + // First Row
-        "<tr><td class='font-weight-bold'> Team Name: </td><td>" +
-        obj.TeamName + "</td></tr>" + // Second Row
-        "<tr><td class='font-weight-bold'> League: </td><td>" +
-        obj.League + "</td></tr>" + // Third Row
-        "<tr><td class='font-weight-bold'> Manager Name: </td><td>" +
-        obj.ManagerName + "</td></tr>" + // Fourth Row
-        "<tr><td class='font-weight-bold'> Manager Phone Number:</td><td>" +
-        obj.ManagerPhone + "</td></tr>" + // Fifth Row
-        "<tr><td class='font-weight-bold'> Manager Email:</td><td>" +
-        obj.ManagerEmail + "</td></tr>" + // Sixth Row
-        "<tr><td class='font-weight-bold'> Max Players:</td><td>" +
-        obj.MaxTeamMembers + "</td></tr>" + // Seventh Row
+        "<tr><td colspan='2' class='text-center font-weight-bold text-uppercase'>" + obj.TeamName + "</td></tr>" + // Second Row
+        "<tr><td class='font-weight-bold'> League: </td><td>" +obj.League + "</td></tr>" +
+        "<tr><td class='font-weight-bold'> Manager Information: </td><td>" +
+        obj.ManagerName + '<br>' + obj.ManagerPhone + '<br>' + obj.ManagerEmail + "</td></t>" +
+        "<tr><td class='font-weight-bold'> Team Rules: </td><td>" +
+        'Ages ' + obj.MinMemberAge + '-' + obj.MaxMemberAge + '<br>' + 'Number of Players ' + obj.MaxTeamMembers + "</td></t>" +
         "<tr><td class='font-weight-bold'> Team Gender:</td><td>" +
-        obj.TeamGender + "</td></tr>"; // Eight Row
+        obj.TeamGender + "</td></tr>";
 
     $("#detailsTable").append(details);
 
@@ -48,33 +41,29 @@ function teamDetails(obj) {
 // Table for Players
 function playersDetails(obj, TeamId) {
     //Table header
-    let playersHeader = 
-    `<tr>
+    let playersHeader = `<tr>
     <th>Player Name</th>
     <th>Email Address</th>
     <th>Action</th></tr>`;
     $("#playersHeader").append(playersHeader);
     // End of table header
 
-    // Loop  thru the array of students
+    // Loop  thru the array of players
     for (let i = 0; i < obj.Members.length; i++) {
         // Table body for students
         let uri = "deletestudent.html?TeamId=" + TeamId + "&studentName=" + obj.Members[i].MemberName + "&email=" + obj.Members[i].Email;
         uri = encodeURI(uri);
         let players = `<tr>
+
         <td> ${obj.Members[i].MemberName}</td>
-        <td> ${obj.Members[i].Email }</td>
-        <td> $<a href=" + uri + ">Unregister<a>" + "</td>
+        <td> ${obj.Members[i].Email}</td>
+        <td> <a href='${uri}'>Unregister</a></td>
         </tr>`;
         $("#playersTable").append(players);
     }
 
-       // Hide if there are not registered players
-       if (obj.Members.length == "") {
+    // Hide if there are not registered players
+    if (obj.Members.length == "") {
         $("#studentsHide").hide();
     }
 }
-
-
-
-
--- public/scripts/leagues.js
+++ public/scripts/leagues.js
@@ -1,52 +1,78 @@
 // Onload function
 "use strict";
 $(function () {
-    let objs;
-    let leaguesObjs;
-    // get data from JSON file
-    $.getJSON('/api/leagues', function (data) {
-     objs = data;
-    // Create my dropdown information from api/categories
+  let objs;
+  let leaguesObjs;
+  // get data from JSON file
+  $.getJSON('/api/leagues', function (data) {
+    objs = data;
+    // Create my dropdown information from api/leagues
     for (let i = 0; i < objs.length; i++) {
       let text = objs[i].Name;
       let value = objs[i].Code;
-      let mydropDownOption = $("<option>", {
-        value: value,
-        text: text
-      });
+      let mydropDownOption = $("<option>",
+        {
+          value: value,
+          text: text
+        });
       mydropDownOption.appendTo("#leaguesddl");
     }
-    });
+  });
   // Onchange function
-  $("#leaguesddl").change(function () {
-    // Clear my table header and table body when the student selects another course. 
+  $("#leaguesddl").change(function () 
+  {
+    // Clear my table header and table body when user selects another league
     $("#teamTable").empty();
     $("#teamHeader").empty();
     $.getJSON("/api/teams/byleague/" + $("#leaguesddl").val(), function (data) {
-      
+
       leaguesObjs = data;
-
-      //Table header starts
-      let teamsHeader = $(
-        "<tr><th>Team Name</th><th>Manager</th><th>Phone Number</th><th>&nbsp;</th><th>&nbsp;</th></tr>");
-
-      $("#teamHeader").append(teamsHeader);
-      // End of table header
-
+      // Call my create a header function
+      createHeader()
       for (let i = 0; i < leaguesObjs.length; i++) {
 
         //Table Body
-        let str = "<tr><td>" + leaguesObjs[i].TeamName + "</td><td>" + leaguesObjs[i].ManagerName+ "</td><td>"
-         + leaguesObjs[i].ManagerPhone+ "</td><td>"+"<a href=details.html?TeamId=" + leaguesObjs[i].TeamId + ">Details<a>" + "</td><td>"
-          +"<a href=edit.html?TeamId="+ leaguesObjs[i].TeamId + ">Edit<a></td></tr>";
+        let str = "<tr><td>" + leaguesObjs[i].TeamName + "</td><td>" + leaguesObjs[i].ManagerName + "</td><td>"
+          + leaguesObjs[i].ManagerPhone + "</td><td>" + "<a href=details.html?TeamId=" + leaguesObjs[i].TeamId + ">Details<a>" + "</td><td>"
+          + "<a href=edit.html?TeamId=" + leaguesObjs[i].TeamId + ">Edit<a></td></tr>";
 
         $("#teamTable").append(str);
 
       }//End of body table
     }); // end of get JSON
   }); //End of my onchange function  
-     
+
+  $("#showAll").on("click", function () {
+    // Clear my table header and table 
+    $("#teamTable").empty();
+    $("#teamHeader").empty();
+    $.getJSON("/api/teams", function (
+      data
+    ) {
+      leaguesObjs = data;
+
+      createHeader()
+
+      for (let i = 0; i < leaguesObjs.length; i++) {
+
+        //Table Body
+        let str = "<tr><td>" + leaguesObjs[i].TeamName + "</td><td>" + leaguesObjs[i].ManagerName + "</td><td>"
+          + leaguesObjs[i].ManagerPhone + "</td><td>" + "<a href=details.html?TeamId=" + leaguesObjs[i].TeamId + ">Details<a>" + "</td><td>"
+          + "<a href=edit.html?TeamId=" + leaguesObjs[i].TeamId + ">Edit<a></td></tr>";
+
+        $("#teamTable").append(str);
+
+      }//End of body table
+    }); // end of get JSON
+  });
 });
+
+// Function create a Header
+function createHeader() {
+  let teamHeader = $(
+    "<tr><th>Team Name</th><th>Manager</th><th>Phone Number</th><th>&nbsp;</th><th>&nbsp;</th></tr>")
+  $("#teamTable").append(teamHeader);
+}
 
 
 
