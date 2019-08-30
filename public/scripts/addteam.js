808a9bcffdd5d0a06145be6ada4673f5b439dd21
Vanessa_Arce <vanessa.arce12@yahoo.com>
2019-08-29 22:16:05 -0400

    added add a team script

--- public/scripts/addteam.js
+++ public/scripts/addteam.js
@@ -1,18 +1,19 @@
 "use strict";
 $(function () {
- 
-  // get data from JSON file for my dropdownlist
+
+    // get data from JSON file for my dropdownlist
     getJSON();
 
+
+    $("#addBtn").on("click", function () {
+    $.post("/api/teams", $("#newTeam").serialize(), function (data) {
+
+        data = JSON.parse(data);
+
+        location.href = "details.html?TeamId=" + data.TeamId;
+
+            })
+        return false;
+    }); 
+
 });
-
-$.post("/api/teams", $("#newTeam").serialize(), function(data) {
-
-data = JSON.parse(data);
-
-location.href = "teamdetails.html?teamid=" + data.TeamId;
-
-});
-
-// end of post
-
