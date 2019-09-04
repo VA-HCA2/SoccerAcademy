d4d5cef955575abe9899d32cc5eaf51d346f41cd
Vanessa_Arce <vanessa.arce12@yahoo.com>
2019-09-04 01:05:26 -0400

    fixed edit validation

--- public/scripts/edit.js
+++ public/scripts/edit.js
@@ -2,12 +2,27 @@
 $(function () {
   let urlParams = new URLSearchParams(location.search);
   let TeamId = urlParams.get("TeamId");
-  let teams; 
+  let teams;
+  let checkMinAge=1000;
+  let checkMaxAge=0;
+
 
   $.getJSON("api/teams/" + TeamId, function (data) {
     teams = data;
     loadTeamData(teams);
-   
+    
+     for ( let i = 0; i < teams.Members.length; i++){
+      if (teams.Members[i].Age < checkMinAge)
+      {
+        checkMinAge=teams.Members[i].Age
+      }
+
+      if (teams.Members[i].Age > checkMaxAge)
+      {
+        checkMaxAge=teams.Members[i].Age
+      }
+    }
+
   }); // end of get JSON
 
   $("#errorTeam").hide();
@@ -15,18 +30,17 @@ $(function () {
   $("#resetBtn").on("click", function () {
 
     loadTeamData(teams)
- });
+  });
 
   $("#editBttn").on("click", function () {
 
-    let isvalidateTeam= validateFormTeam();
+    let isvalidateTeam = validateFormTeam(teams,checkMinAge,checkMaxAge);
 
-     if (isvalidateTeam == false)
-     {
-        $("#errorTeam").show()
-        return ;
-     }
-     
+    if (isvalidateTeam == false) {
+      $("#errorTeam").show()
+      return;
+    }
+
     $.ajax({
       url: '/api/teams',
       data: $("#editTeam").serialize(),
@@ -42,82 +56,97 @@ $(function () {
 
     window.location.href = "details.html?TeamId=" + TeamId;
   });
-  
+
 }); // end of ready fuction
 
 
-function loadTeamData(teams){
+function loadTeamData(teams) {
 
-    $("#teamid").val(teams.TeamId);
-    $("#leaguecode").val(teams.League);
-    $("#teamName").val(teams.TeamName);
-    $("#managername").val(teams.ManagerName);
-    $("#managerphone").val(teams.ManagerPhone);
-    $("#manageremail").val(teams.ManagerEmail);
-    $("#teammembers").val(teams.MaxTeamMembers);
-    $("#minmemberage").val(teams.MinMemberAge);
-    $("#maxmemberage").val(teams.MaxMemberAge);
-    $("#teamgender").val(teams.TeamGender);
+  $("#teamid").val(teams.TeamId);
+  $("#leaguecode").val(teams.League);
+  $("#teamName").val(teams.TeamName);
+  $("#managername").val(teams.ManagerName);
+  $("#managerphone").val(teams.ManagerPhone);
+  $("#manageremail").val(teams.ManagerEmail);
+  $("#teammembers").val(teams.MaxTeamMembers);
+  $("#minmemberage").val(teams.MinMemberAge);
+  $("#maxmemberage").val(teams.MaxMemberAge);
+  $("#teamgender").val(teams.TeamGender);
 }
 
 
 // Function to Validate forms
-function validateFormTeam() {
+function validateFormTeam(teams,checkMinAge,checkMaxAge) {
   let pattern = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
-  let phonepattern= /^1?[-\. ]?(\(\d{3}\)?[-\. ]?|\d{3}?[-\. ]?)?\d{3}?[-\. ]?\d{4}$/
+  let phonepattern = /^1?[-\. ]?(\(\d{3}\)?[-\. ]?|\d{3}?[-\. ]?)?\d{3}?[-\. ]?\d{4}$/
   let errMsg = [];
 
   $("#errorTeam").hide()
 
   if ($("#teamName").val().trim() == "") {
-      errMsg[errMsg.length] = "Team Name is required";
+    errMsg[errMsg.length] = "Team Name is required";
   }
 
   if ($("#managername").val().trim() == "") {
-      errMsg[errMsg.length] = "Manager name is required";
+    errMsg[errMsg.length] = "Manager name is required";
   }
 
   if ($("#manageremail").val().trim() == "") {
-      errMsg[errMsg.length] = "Manager E-mail is required";
+    errMsg[errMsg.length] = "Manager E-mail is required";
   }
   else if (!pattern.test($("#manageremail").val().trim())) {
-      errMsg[errMsg.length] = ("Please enter a valid Email-Address field");
+    errMsg[errMsg.length] = ("Please enter a valid Email-Address field");
   }
 
   if ($("#teammembers").val().trim() == "") {
-      errMsg[errMsg.length] = "Number of players is required";
+    errMsg[errMsg.length] = "Number of players is required";
+  }
+
+  else if ($("#teammembers").val() < teams.Members.length) {
+    errMsg[errMsg.length] = "Maximum Number of players not allowed. There are more players already registered on that Team";
   }
 
   if ($("#managerphone").val().trim() == "") {
-      errMsg[errMsg.length] = "Phone Number is required";
+    errMsg[errMsg.length] = "Phone Number is required";
   }
-  else if (!phonepattern.test($("#managerphone").val().trim())){
-      errMsg[errMsg.length] = ("Phone enter a valid Phone Number");
+  else if (!phonepattern.test($("#managerphone").val().trim())) {
+    errMsg[errMsg.length] = ("Phone enter a valid Phone Number");
   }
 
   if ($("#minmemberage").val().trim() == "") {
-      errMsg[errMsg.length] = " Minimum age is required";
+    errMsg[errMsg.length] = " Minimum age is required";
   }
 
-  else if ($("#minmemberage").val() <= 0) {
-      errMsg[errMsg.length] = "Minimum age must be greater than 0";
+  else if (isNaN($("#minmemberage").val()) || $("#minmemberage").val() <= 0) {
+    errMsg[errMsg.length] = "Please enter a valid Age";
   }
 
-  // Max age validation 
+  else if ( $("#minmemberage").val() > checkMinAge ) {
+    errMsg[errMsg.length] = "Existing team players don't meet Minimum Member Age. Youngest player is "+checkMinAge;
+  }
 
+  // Max Age
   if ($("#maxmemberage").val().trim() == "") {
-      errMsg[errMsg.length] = "Maximum age is required";
+    errMsg[errMsg.length] = " Maximum age is required";
+  }
+
+  else if (isNaN($("#maxmemberage").val()) || $("#maxmemberage").val() <= 0) {
+    errMsg[errMsg.length] = "Please enter a valid Age";
+  }
+
+  else if ( $("#maxmemberage").val() < checkMaxAge ) {
+    errMsg[errMsg.length] = "Existing team players don't meet Maximum Member Age. Older player is "+checkMaxAge;
   }
 
   $("#errorTeam").empty();
   for (let i = 0; i < errMsg.length; i++) {
-      $("<li>" + errMsg[i] + "</li>").appendTo("#errorTeam");
-  }
- 
-  if (errMsg.length >0){
-      return false
+    $("<li>" + errMsg[i] + "</li>").appendTo("#errorTeam");
   }
 
- return true;
+  if (errMsg.length > 0) {
+    return false
+  }
+
+  return true;
 
 }
--- public/scripts/register.js
+++ public/scripts/register.js
@@ -4,25 +4,37 @@ $(function () {
     let urlParams = new URLSearchParams(location.search);
     let teamId = urlParams.get("TeamId");
     let objs;
+
     // Get team name from api teams to generate in form 
     $.getJSON("api/teams/" + teamId, function (data) {
+
         objs=data;
 
         $("#teamName").val(data.TeamName);
+        $("#gender").val(data.TeamGender);
+
+        if (objs.Members.length >= objs.MaxTeamMembers )
+        {
+            $("#registerBtn").prop("disabled",true);
+            $("#numOfPlayers").show();
+            $("#numOfPlayers").html("Registration not allowed.Maximum Number of Players have been reach!");
+        }
     });
 
     $("#error").hide();
     // On Click Function 
+
+         // On Click Function 
     $("#registerBtn").on("click", function () {
 
-        let isvalidate= validateForm();
+        let isvalidate= validateForm(objs);
 
         if (isvalidate == false)
         {
             $("#error").show();
             return ;
         }
-        // If data is entered post in the students table  
+        // If data is entered post in the players table  
         $.post(`/api/teams/${teamId}/members`, $("#newPlayer").serialize(), function (data) {
             window.location.href = "details.html?TeamId=" + teamId;
         })
@@ -49,3 +61,68 @@ $(function () {
 }); // end of ready fuction
 
 
+// Function to Validate data for Register Form 
+function validateForm(objs) {
+    let pattern = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+    let phonepattern = /^1?[-\. ]?(\(\d{3}\)?[-\. ]?|\d{3}?[-\. ]?)?\d{3}?[-\. ]?\d{4}$/
+    let errMsg = [];
+
+
+    if ($("#memberName").val().trim() == "") {
+        errMsg[errMsg.length] = "Name is required";
+    }
+
+    if ($("#contactName").val().trim() == "") {
+        errMsg[errMsg.length] = "Contact name is required";
+    }
+
+    if ($("#email").val().trim() == "") {
+        errMsg[errMsg.length] = "Email is required";
+    }
+    else if (!pattern.test($("#email").val().trim())) {
+        errMsg[errMsg.length] = ("Please enter a valid Email-Address field");
+    }
+
+    if ($("#phone").val().trim() == "") {
+        errMsg[errMsg.length] = "Phone Number is required";
+    }
+    else if (!phonepattern.test($("#phone").val().trim())) {
+        errMsg[errMsg.length] = ("Phone enter a valid Phone Number");
+    }
+
+    if ($("#age").val().trim() == "") {
+        errMsg[errMsg.length] = "Age is required";
+    }
+
+    else if(isNaN($("#age").val()) || $("#age").val() <= 0) {
+        errMsg[errMsg.length] = "Please enter a valid Age";
+    }
+
+    else if( $("#age").val() < objs.MinMemberAge ){
+        errMsg[errMsg.length] = "Player does not meet Minium Age requirements.Minimum Age required " +objs.MinMemberAge;
+    }
+
+    else if( $("#age").val() > objs.MaxMemberAge){
+        errMsg[errMsg.length] = "Player does not meet Maximum Age requirements. Maximum Age required "+objs.MaxMemberAge;
+    }
+
+    if ($("input[name='gender']:checked").val() == null)
+    {
+        errMsg[errMsg.length] = "Gender is required";
+    }
+    
+    else if ($("input[name='gender']:checked").val() != objs.TeamGender)
+     {
+            errMsg[errMsg.length]= "Player's Gender not allowed on this Team.";
+    }
+
+    $("#error").empty();
+    for (let i = 0; i < errMsg.length; i++) {
+        $("<li>" + errMsg[i] + "</li>").appendTo("#error");
+    }
+    
+    if (errMsg.length > 0) {
+        return false
+    }
+    return true;
+}
