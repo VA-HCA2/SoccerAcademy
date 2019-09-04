467cc5c5f70d649ba71c1969b7490746b90a8a3c
Vanessa_Arce <vanessa.arce12@yahoo.com>
2019-09-04 14:16:54 -0400

    fixes

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -11,13 +11,12 @@ $(function () {
     // Get team name from api teams to generate in form 
     $.getJSON("api/teams/" + teamId, function (data) {
 
-        objs=data;
-    // Populate information 
+        objs = data;
+        // Populate information 
         $("#teamName").val(data.TeamName);
-    // Check if max number of players have been reach.
-        if (objs.Members.length >= objs.MaxTeamMembers )
-        {
-    // If it's over hide registration button and show message. 
+        // Check if max number of players have been reach.
+        if (objs.Members.length >= objs.MaxTeamMembers) {
+            // If it's over hide registration button and show message. 
             $("#registerBtn").hide();
             $("#numOfPlayers").show();
             $("#numOfPlayers").html("Registration not allowed.Maximum Number of Players have been reach!");
@@ -26,15 +25,14 @@ $(function () {
     // Hide error 
     $("#error").hide();
 
-         // On Click Function 
+    // On Click Function 
     $("#registerBtn").on("click", function () {
         // Call validate function 
-        let isvalidate= validateForm(objs);
+        let isvalidate = validateForm(objs);
 
-        if (isvalidate == false)
-        {
+        if (isvalidate == false) {
             $("#error").show();
-            return ;
+            return;
         }
         // If data is entered post in the players table  
         $.post(`/api/teams/${teamId}/members`, $("#newPlayer").serialize(), function (data) {
@@ -55,7 +53,7 @@ $(function () {
         $("#age").val("");
         $("#gender").val("Male");
     });
-   
+
     // Reset button on click event. 
     $("#returnBtn").on("click", function () {
 
@@ -81,49 +79,52 @@ function validateForm(objs) {
     if ($("#email").val().trim() == "") {
         errMsg[errMsg.length] = "Email is required";
     }
-     // Validate with reg expressions.  
+    // Validate with reg expressions.  
     else if (!pattern.test($("#email").val().trim())) {
         errMsg[errMsg.length] = ("Please enter a valid Email-Address field");
     }
-     // Validate phone number. 
+    // Validate phone number. 
     if ($("#phone").val().trim() == "") {
         errMsg[errMsg.length] = "Phone Number is required";
     }
-     // Validate phone with reg expressions. 
+    // Validate phone with reg expressions. 
     else if (!phonepattern.test($("#phone").val().trim())) {
         errMsg[errMsg.length] = ("Enter a valid Phone Number");
     }
-     // Validate if age is empty. 
+    // Validate if age is empty. 
     if ($("#age").val().trim() == "") {
         errMsg[errMsg.length] = "Age is required";
     }
-     // Validate if age is not a number or if it's less than 0.   
-    else if(isNaN($("#age").val()) || Number($("#age").val() <= 0)) {
+    // Validate if age is not a number or if it's less than 0.   
+    else if (isNaN($("#age").val()) || Number($("#age").val() <= 0)) {
         errMsg[errMsg.length] = "Please enter a valid Age";
     }
     // Validate if age is player doesn't meet min age requirements. 
-    else if( $("#age").val() < objs.MinMemberAge ){
-        errMsg[errMsg.length] = "Player does not meet Minium Age requirements.Minimum Age required " +objs.MinMemberAge;
+    else if ($("#age").val() < objs.MinMemberAge) {
+        errMsg[errMsg.length] = "Player does not meet Minium Age requirements.Minimum Age required " + objs.MinMemberAge;
     }
     // Validate if age is player doesn't meet max age requirements. 
-    else if( $("#age").val() > objs.MaxMemberAge){
-        errMsg[errMsg.length] = "Player does not meet Maximum Age requirements. Maximum Age required "+objs.MaxMemberAge;
+    else if ($("#age").val() > objs.MaxMemberAge) {
+        errMsg[errMsg.length] = "Player does not meet Maximum Age requirements. Maximum Age required " + objs.MaxMemberAge;
     }
     // Validate if Gender is not selected.  
-    if ($("input[name='gender']:checked").val() == null)
-    {
+    if ($("input[name='gender']:checked").val() == null) {
         errMsg[errMsg.length] = "Gender is required";
     }
     // Validate if Gender is not equal to the team requirements. 
-    
-    else if ($("input[name='gender']:checked").val() != objs.TeamGender )
-      errMsg[errMsg.length]= "Player's Gender not allowed on this Team.";
-  
+
+    else if (($("input[name='gender']:checked").val() != objs.TeamGender) && ($("input[name='gender']:checked").val() == null)) {
+        if ($("input[name='gender']:checked").val() != "Any") {
+            errMsg[errMsg.length] = "Player's Gender not allowed on this Team.";
+        }
+    }
+
+
     $("#error").empty();
     for (let i = 0; i < errMsg.length; i++) {
         $("<li>" + errMsg[i] + "</li>").appendTo("#error");
     }
-    
+
     if (errMsg.length > 0) {
         return false
     }
